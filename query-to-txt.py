856659bfe3c9b93a79c4d6addb19ed68d45f92aa
ycho <ycho@utexas.edu>
2022-09-18 22:01:08 -0500

    query input to text data output

---
+++ query-to-txt.py
@@ -0,0 +1,79 @@
+from src import ContextDoc
+from collections import defaultdict
+import pickle as pkl
+from tqdm import tqdm
+import os
+import sys
+
+def collect_sample(sent, query, query_pos):
+    lemmas_in_sent = [w.lemma.lower() for w in sent.words]
+    try:
+        i = lemmas_in_sent.index(query)
+    except ValueError:
+        return None
+
+    try:
+        word_pos = sent.words[i].pos
+    except IndexError:
+        return None
+
+    if word_pos == query_pos:
+        return sent.text
+    else:
+        return None
+
+
+query = sys.argv[1]
+
+verbose = False
+query_pos = "NOUN"
+dict_path = "pkl/coca-fiction-dict.pkl"
+
+coca_dict = defaultdict(list)
+print('Query:', query)
+with open("pkl/coca-fiction-stanza.pkl", "rb") as fr:
+    try:
+        while True:
+            doc = pkl.load(fr)
+            for i_sent, sent in enumerate(tqdm(doc.sentences, leave=False)):
+                sample = collect_sample(sent, query, query_pos)
+                if sample:
+                    if verbose:
+                        print(sent.text)
+                    try:
+                        sent_before = doc.sentences[i_sent-1].text
+                    except IndexError:
+                        sent_before = ''
+                    try:
+                        sent_next = doc.sentences[i_sent+1].text
+                    except IndexError:
+                        sent_next = ''
+                    sent_curr = sent.text
+
+                    cdoc = ContextDoc(sent_before, sent_curr, sent_next)
+                    coca_dict[query].append(cdoc)
+
+                else:
+                    continue
+    except EOFError:
+        pass
+
+if os.path.exists(dict_path):
+    with open(dict_path, "rb") as exdict:
+        existing_dict = pkl.load(exdict)
+        existing_dict[query] = coca_dict[query]
+
+    new_dict = existing_dict
+    with open(dict_path, "wb") as f:
+        pkl.dump(new_dict, f)
+else:
+    with open(dict_path, "wb") as f:
+        pkl.dump(coca_dict, f)
+
+
+with open("results/" + query + ".txt", "w") as f:
+    for doc in coca_dict[query]:
+        line = ' | '.join([doc.sent_before, doc.sent_curr, doc.sent_next])
+        f.writelines(line + '\n')
+
+
--- stat.py
+++ stat.py
@@ -3,6 +3,8 @@ import pickle as pkl
 # stat
 doc_cnt = 0
 sent_cnt = 0
+word_cnt = 0
+token_cnt = 0
 
 with open("pkl/coca-fiction-stanza.pkl", "rb") as fr:
     try:
@@ -10,9 +12,13 @@ with open("pkl/coca-fiction-stanza.pkl", "rb") as fr:
             doc = pkl.load(fr)
             doc_cnt += 1
             sent_cnt += len(doc.sentences)
+            word_cnt += doc.num_words
+            token_cnt += doc.num_tokens
     except EOFError:
         pass
 
 print("Total # of documents: {}".format(str(doc_cnt)))
 print("Total # of sentences: {}".format(str(sent_cnt)))
+print("Total # of words: {}".format(str(word_cnt)))
+print("Total # of tokens: {}".format(str(token_cnt)))
 
